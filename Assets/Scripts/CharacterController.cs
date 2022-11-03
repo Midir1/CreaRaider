f1bfaec42f898243b2b613a069d4f0fc2d096a99
Midir1 <60351234+Midir1@users.noreply.github.com>
2022-11-03 12:30:55 +0100

    Made Character Jump

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -5,13 +5,25 @@ public class CharacterController : MonoBehaviour
     [Header("Character Parameters")]
     [SerializeField] private float movementSpeed;
     [SerializeField] private float rotationSpeed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float sideJumpForce;
+    [SerializeField] private float fallSpeed;
     
+    [Header("Raycast Parameters")]
+    [SerializeField] private Transform raycastOrigin;
+    [SerializeField] private float raycastMaxDistance;
+
     private Rigidbody _rb;
 
     private Vector3 _rotationVelocity;
 
+    private bool _isGrounded;
+
+    private const int LayerMask = 1 << 6;
+
     private const string HorizontalInput = "Horizontal", VerticalInput = "Vertical";
     private const string MouseInput = "Mouse X";
+    private const string JumpInput = "Jump";
 
     private void Start()
     {
@@ -22,23 +34,45 @@ public class CharacterController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        CharacterMovement();
-        CharacterRotation();
-    }
-
-    private void CharacterMovement()
-    {
         float horizontal = Input.GetAxisRaw(HorizontalInput) * movementSpeed;
         float vertical = Input.GetAxisRaw(VerticalInput) * movementSpeed;
         
+        float mouseAxis = Input.GetAxisRaw(MouseInput);
+        float jumpAxis = Input.GetAxisRaw(JumpInput);
+        
+        CharacterMovement(horizontal, vertical);
+        CharacterRotation(mouseAxis);
+        CharacterJump(jumpAxis, horizontal, vertical);
+        CharacterFall();
+        
+        CheckCharacterGrounded();
+    }
+
+    private void CharacterMovement(float horizontal, float vertical)
+    {
         _rb.AddForce(transform.forward * vertical + transform.right * horizontal, ForceMode.Force);
     }
 
-    private void CharacterRotation()
+    private void CharacterRotation(float mouseAxis)
     {
-        float mouseAxis = Input.GetAxisRaw(MouseInput);
         Quaternion deltaRotation = Quaternion.Euler(_rotationVelocity * (Time.fixedDeltaTime * mouseAxis));
         
         _rb.MoveRotation(_rb.rotation * deltaRotation);
+    }
+
+    private void CharacterJump(float jumpAxis, float horizontal, float vertical)
+    {
+        if (_isGrounded && jumpAxis != 0f) 
+            _rb.AddForce(transform.up * (jumpForce * jumpAxis) + (transform.forward * (sideJumpForce * vertical) + transform.right * (sideJumpForce * horizontal)).normalized, ForceMode.Impulse);
+    }
+
+    private void CharacterFall()
+    {
+        if(!_isGrounded) _rb.velocity -= new Vector3(0f, fallSpeed, 0f);
+    }
+    
+    private void CheckCharacterGrounded()
+    {
+        _isGrounded = Physics.Raycast(raycastOrigin.position, Vector3.down, out _, raycastMaxDistance, LayerMask);
     }
 }
